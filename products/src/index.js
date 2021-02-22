d72367c2f45ae4330ef86ed2d74d7f3e9b81ec29
Niels Koster <n.koster@portavita.eu>
2021-02-23 00:22:24 +0100

    bootstrap logic to make sure shared module is recognized by webpack

--- products/src/index.js
+++ products/src/index.js
@@ -1,9 +1,1 @@
-import faker from 'faker'
-
-let products = ''
-
-for (let i = 0; i < 10; i++) {
-    products += `<div>${faker.commerce.productName()}</div>\n`
-}
-
-document.querySelector('#dev-products').innerHTML = products
+import('./bootstrap')
